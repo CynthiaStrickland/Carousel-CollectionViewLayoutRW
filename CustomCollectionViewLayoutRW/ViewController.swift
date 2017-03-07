e96844cc5f2424e887808b9eae33cdd6df784aa2
Cynthia Strickland <cynthiastrickland001@gmail.com>
2017-03-07 13:30:29 -0800

    Initial Commit

---
+++ CustomCollectionViewLayoutRW/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomCollectionViewLayoutRW
+//
+//  Created by Cynthia Whitlatch on 3/7/17.
+//  Copyright © 2017 Fenix Design. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
